18c62d8e0a210b208ae7167f06ebdf4f8606ebef
Ian Senne <ian@iansenne.com>
2023-11-30 15:16:28 -0800

    set up npm package

--- scripts/compile-output.ts
+++ scripts/compile-output.ts
@@ -1,13 +1,17 @@
 import { build } from "esbuild";
 
 const outputs = [process.argv[2]];
-
+let header = "";
+if (process.argv[2] === "bin/mcb.js") {
+  header = "#!/usr/bin/env node";
+}
 build({
   entryPoints: outputs,
   bundle: true,
   outdir: "dist",
   platform: "node",
   minifySyntax: false,
+  banner: { js: header },
   // minifyWhitespace: true,
   // minifyIdentifiers: true,
 });
