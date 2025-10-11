df2bf2ba1992dca0bd6147933b1d4f6cd7fa7bbc
Ian Senne <IanSSenne@gmail.com>
2025-10-11 03:51:04 -0700

    remove haxe implimentation.

--- setup.sh
+++
@@ -1,7 +0,0 @@
-npm i
-rm -rf ./.haxelib
-npm run setup-deps
-haxelib install lib.hxml --always
-# cp ./overrides/genes/src/genes/Register.hx ./.haxelib/genes/0,4,13/src/genes/Register.hx
-cd ./.haxelib/haxpression
-find . -name '*.hx' -type f -exec sed -i 's/operator/operator_/g' {} \;
--- src/ext/Module.hx
+++
@@ -1,6 +0,0 @@
-package ext;
-
-@:jsRequire("module")
-extern class Module {
-	public extern static function createRequire(file:String):String->Any;
-}
